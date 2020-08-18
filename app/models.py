6380478e72feb1b157c7e9066c7bc0cc20928795
Sean Morgan <sean@roguelement.org>
2020-08-18 17:52:20 -0400

    minor error

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from app import db
-
+from datetime import datetime
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
